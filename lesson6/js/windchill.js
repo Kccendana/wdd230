09b0df8d88b2185113c8791c9e9012a209631cdf
Kccendana <kc_telyn@yahoo.com>
2021-10-22 23:14:56 +0900

    lesson6

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let temp = parseFloat(document.querySelector(".highTemp").textContent);
+    let wSpeed = parseFloat(document.querySelector(".windSpeed").textContent);
+    let result = windChill(temp, wSpeed);
+    
+    document.querySelector(".windChill").innerHTML = result;
+   
+});
+
+function windChill(temp, wSpeed){
+
+    if (temp <= 50 && wSpeed > 3){
+       let chill = 35.74 + 0.6215 * temp - 35.75 * 
+                Math.pow(wSpeed, 0.16) + 0.4275 * temp * Math.pow(wSpeed, 0.16);
+        chill = chill.toFixed(0) + "&deg;F";        
+                return chill;
+    }else{return "N/A";}
+}
