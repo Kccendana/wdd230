e58ffd64a11eecd55fc12522b538dc56e5935677
Kccendana <kc_telyn@yahoo.com>
2021-10-29 08:58:26 +0900

    final

--- lesson-7/js/javascript.js
+++ lesson-7/js/javascript.js
@@ -8,25 +8,6 @@ const loadImages = (image) => {
 imagesToLoad.forEach((img) => {
   loadImages(img);
 });
-
-if('IntersectionObserver' in window) {
-  const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-      if(item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  });
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-}
-
 if('IntersectionObserver' in window) {
   const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
